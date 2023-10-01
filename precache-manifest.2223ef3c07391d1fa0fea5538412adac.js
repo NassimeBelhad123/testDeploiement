7af7738ea22e5714534e12808537eddd851512f0
Nassime Belhad <bakunass@hotmail.com>
2023-10-01 04:15:46 -0400

    Updates

---
+++ precache-manifest.2223ef3c07391d1fa0fea5538412adac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b8044ae30ac0840356b5bfe9b91c8f",
+    "url": "/NassimeBelhad123/testDeploiement/index.html"
+  },
+  {
+    "revision": "07936c9136d9df909a29",
+    "url": "/NassimeBelhad123/testDeploiement/static/css/main.947f9ed5.chunk.css"
+  },
+  {
+    "revision": "2cd54ab4af9fadb7f2a9",
+    "url": "/NassimeBelhad123/testDeploiement/static/js/2.f3f1a76b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/NassimeBelhad123/testDeploiement/static/js/2.f3f1a76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07936c9136d9df909a29",
+    "url": "/NassimeBelhad123/testDeploiement/static/js/main.2f428603.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/NassimeBelhad123/testDeploiement/static/js/main.2f428603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64c46bed1b53edb677e",
+    "url": "/NassimeBelhad123/testDeploiement/static/js/runtime-main.d276f01d.js"
+  }
+]);
